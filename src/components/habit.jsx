6c33b287d90710094ae28b1c6f9e05639d934e29
MEK <ansenfrl@gmail.com>
2020-11-27 15:03:06 +0900

    habits 기능 완료

--- src/components/habit.jsx
+++ src/components/habit.jsx
@@ -1,34 +1,29 @@
 import React from "react";
 
-const Habit = ({ habit }) => {
-  console.log(habit);
-  //   const handleIncrement = () => {
-  //     setCount(habit.count + 1);
-  //   };
-  //   const handledecrement = () => {
-  //     setCount(habit.count - 1 < 0 ? 0 : habit.count - 1);
-  //   };
-  //   const handleDelete = () => {};
+const Habit = (props) => {
+  const handleIncrement = () => {
+    props.onIncrement(props.habit);
+  };
+  const handleDecrement = () => {
+    props.onDecrement(props.habit);
+  };
+  const handleDelete = () => {
+    props.onDelete(props.habit);
+  };
 
   return (
     <li className="habit">
-      <span className="habit-name">{habit.name}</span>
-      <span className="habit-count">{habit.count}</span>
-      <button
-        className="habit-button habit-increase"
-        //   onClick={handleIncrement}
-      >
+      <span className="habit-name">{props.habit.name}</span>
+      <span className="habit-count">{props.habit.count}</span>
+      <button className="habit-button habit-increase" onClick={handleIncrement}>
         <i className="fas fa-plus-square"></i>
       </button>
 
-      <button
-        className="habit-button habit-decrease"
-        //   onClick={handledecrement}
-      >
+      <button className="habit-button habit-decrease" onClick={handleDecrement}>
         <i className="fas fa-minus-square"></i>
       </button>
 
-      <button className="habit-button habit-delete">
+      <button className="habit-button habit-delete" onClick={handleDelete}>
         <i className="fas fa-trash"></i>
       </button>
     </li>
--- src/components/habits.jsx
+++ src/components/habits.jsx
@@ -3,14 +3,47 @@ import Habit from "./habit";
 
 const Habits = () => {
   const [habits, setCount] = useState([
-    { id: 1, name: "1", count: 2 },
-    { id: 2, name: "2", count: 2 },
-    { id: 3, name: "3", count: 2 },
+    { id: 1, name: "노드", count: 2 },
+    { id: 2, name: "리액트", count: 2 },
+    { id: 3, name: "알고리즘", count: 2 },
   ]);
+
+  const handleIncrement = (habit) => {
+    const newHabits = [...habits];
+    const index = newHabits.indexOf(habit);
+    newHabits[index].count++;
+    setCount(newHabits);
+  };
+
+  const handleDecrement = (habit) => {
+    const newHabits = [...habits];
+    const index = newHabits.indexOf(habit);
+    const count = newHabits[index].count - 1;
+    newHabits[index].count = count < 0 ? 0 : count;
+    setCount(newHabits);
+  };
+
+  const handleDelete = (habit) => {
+    const newHabits = habits.filter((item) => item.id !== habit.id);
+    setCount(newHabits);
+
+    // habit의 아이디를 가지고,
+    // habits의 아이디랑 비교해보고
+    // filter를 통해서 ( true만 반환 )
+  };
+
   return (
     <ul>
       {habits.map((habit) => {
-        return <Habit key={habit.id} habit={habit} />;
+        return (
+          <Habit
+            key={habit.id}
+            habit={habit}
+            onIncrement={handleIncrement}
+            onDecrement={handleDecrement}
+            onDelete={handleDelete}
+          />
+        );
       })}
     </ul>
   );
