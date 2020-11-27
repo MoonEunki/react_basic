651ecb7d1901b7ab64fd2df51c918fb2521e8f17
MEK <ansenfrl@gmail.com>
2020-11-27 15:17:57 +0900

    App 으로 state 이동

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,40 @@
-import Habit from "./components/habit";
 import "./app.css";
 import Habits from "./components/habits";
+import React, { useState } from "react";
 
-function App() {
-  return <Habits />;
-}
+const App = () => {
+  const [habits, setCount] = useState([
+    { id: 1, name: "노드", count: 2 },
+    { id: 2, name: "리액트", count: 2 },
+    { id: 3, name: "알고리즘", count: 2 },
+  ]);
+  const handleIncrement = (habit) => {
+    const newHabits = [...habits];
+    const index = newHabits.indexOf(habit);
+    newHabits[index].count++;
+    setCount(newHabits);
+  };
+
+  const handleDecrement = (habit) => {
+    const newHabits = [...habits];
+    const index = newHabits.indexOf(habit);
+    const count = newHabits[index].count - 1;
+    newHabits[index].count = count < 0 ? 0 : count;
+    setCount(newHabits);
+  };
+
+  const handleDelete = (habit) => {
+    const newHabits = habits.filter((item) => item.id !== habit.id);
+    setCount(newHabits);
+  };
+  return (
+    <Habits
+      habits={habits}
+      onIncrement={handleIncrement}
+      onDecrement={handleDecrement}
+      onDelete={handleDelete}
+    />
+  );
+};
 
 export default App;
--- src/components/habits.jsx
+++ src/components/habits.jsx
@@ -1,40 +1,22 @@
-import React, { useState } from "react";
+import React from "react";
 import Habit from "./habit";
 
-const Habits = () => {
-  const [habits, setCount] = useState([
-    { id: 1, name: "노드", count: 2 },
-    { id: 2, name: "리액트", count: 2 },
-    { id: 3, name: "알고리즘", count: 2 },
-  ]);
-
+const Habits = (props) => {
   const handleIncrement = (habit) => {
-    const newHabits = [...habits];
-    const index = newHabits.indexOf(habit);
-    newHabits[index].count++;
-    setCount(newHabits);
+    props.onIncrement(habit);
   };
 
   const handleDecrement = (habit) => {
-    const newHabits = [...habits];
-    const index = newHabits.indexOf(habit);
-    const count = newHabits[index].count - 1;
-    newHabits[index].count = count < 0 ? 0 : count;
-    setCount(newHabits);
+    props.onDecrement(habit);
   };
 
   const handleDelete = (habit) => {
-    const newHabits = habits.filter((item) => item.id !== habit.id);
-    setCount(newHabits);
-
-    // habit의 아이디를 가지고,
-    // habits의 아이디랑 비교해보고
-    // filter를 통해서 ( true만 반환 )
+    props.onDelete(habit);
   };
 
   return (
     <ul>
-      {habits.map((habit) => {
+      {props.habits.map((habit) => {
         return (
           <Habit
             key={habit.id}
